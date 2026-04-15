43fea04072a24797a25dcf028cc677910f049b56
wassup-999 <sebasquispeperez@gmail.com>
2026-04-15 08:59:26 -0500

    Update RecursiveClass.cs

--- Assets/Scripts/Semana5AED/RecursiveClass.cs
+++ Assets/Scripts/Semana5AED/RecursiveClass.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 public class RecursiveClass : MonoBehaviour
 {
     
     void Start()
     {
-        //CountDown(5);
-        Debug.Log(Sum(5));
+        
     }
 
     
@@ -32,4 +32,39 @@ public class RecursiveClass : MonoBehaviour
             return 1;
         return n + Sum (n-1);
     }
+    [Button]
+    public void PrintFlow(int n)
+    {
+        if (n < 1)
+            return;
+        Debug.Log("Entrando" + n);
+        print(n - 1);
+        Debug.Log("Saliendo" + n);
+    }
+    [Button]
+    public int SumExpress(int n)
+    {
+        if (n == 1)
+            return 1;
+        return SumExpress(n - 1) + SumExpress(n -1);
+    }
+    [Button]
+    public int Factorial(int n)
+    {
+        if(n == 1)
+            return 1;
+        return n * Factorial(n - 1);
+    }
+
+    [Button]
+    public int Fibonacci(int n)
+    {
+        if(n==0)
+            return 0;
+        if(n == 1)
+            return 1;
+
+        return Fibonacci (n - 1) + Fibonacci(n-2);
+    }
+
 }
